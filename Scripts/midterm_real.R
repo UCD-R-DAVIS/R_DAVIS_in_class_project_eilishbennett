207cc51ea637ffbe594e33d08636b9cbe8a91bde
Eilish Bennett <ejbennett@ucdavis.edu>
2024-10-30 20:08:30 -0700

    Did not get all the way through :(

---
+++ Scripts/midterm_real.R
@@ -0,0 +1,59 @@
+#Midterm
+
+#Read in the file tyler_activity_laps_10-24.csv from the class github page. This file is at url https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/refs/heads/main/data/tyler_activity_laps_10-24.csv, so you can code that url value as a string object in R and call read_csv() on that object. The file is a .csv file where each row is a “lap” from an activity Tyler tracked with his watch.
+
+library(readr)
+tyler_activity <- "https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/refs/heads/main/data/tyler_activity_laps_10-24.csv"
+
+library(tidyverse)
+view(tyler_activity)
+head(tyler_activity)
+
+tyler_activity_data <- read_csv(tyler_activity)
+
+head(tyler_activity_data)
+
+#Filter out any non-running activities.
+tyler_running <- tyler_activity_data %>% 
+  filter(sport == "running")
+head(tyler_running)
+
+#Next, Tyler often has to take walk breaks between laps right now because trying to change how you’ve run for 25 years is hard. You can assume that any lap with a pace above 10 minute-per-mile pace is walking, so remove those laps. You should also remove any abnormally fast laps (< 5 minute-per-mile pace) and abnormally short records where the total elapsed time is one minute or less.
+view(tyler_running)
+str(tyler_running)
+summary(tyler_running$minutes_per_mile)
+
+tyler_running_pace <- tyler_running %>% 
+  filter(minutes_per_mile < 10 & minutes_per_mile > 5) %>% 
+  filter(total_elapsed_time_s > 60)
+view(tyler_running_pace)
+
+head(tyler_running_pace)
+summary(tyler_running_pace$minutes_per_mile)
+summary(tyler_running_pace$total_elapsed_time_s)
+
+#Create a new categorical variable, pace, that categorizes laps by pace: “fast” (< 6 minutes-per-mile), “medium” (6:00 to 8:00), and “slow” ( > 8:00). Create a second categorical variable, form that distinguishes between laps run in the year 2024 (“new”, as Tyler started his rehab in January 2024) and all prior years (“old”).
+
+tyler_running_pace <- tyler_running_pace %>% 
+  mutate(pace = case_when(
+    minutes_per_mile < 6 ~ "fast", 
+    minutes_per_mile >= 6 & minutes_per_mile <= 8 ~ "medium", 
+    T ~ "slow"
+  ))
+
+head(tyler_running_pace$pace_category)
+view(tyler_running_pace)
+
+tyler_running_pace <- tyler_running_pace %>% 
+  mutate(form = case_when(year <= 2023 ~ "old", 
+                          year > 2023 ~ "new"))
+view(tyler_running_pace)
+
+#Identify the average steps per minute for laps by form and pace, and generate a table showing these values with old and new as separate rows and pace categories as columns. Make sure that slow speed is the second column, medium speed is the third column, and fast speed is the fourth column (hint: think about what the select() function does). 
+view(tyler_running_pace$steps_per_minute)
+
+tyler_running_pace <- tyler_running_pace %>% 
+  group_by(form, pace) %>% 
+  mutate(avg_steps = mean(steps_per_minute, na.rm = TRUE)) %>% 
+  select(form, pace, avg_steps)
+tyler_running_pace
